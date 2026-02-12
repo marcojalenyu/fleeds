671d9c75314f1a3d37cfcb75124a8a9670f92f4e
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2026-02-12 21:48:32 +0800

    ui: Pfp/banner options

---
+++ lib/core/constants/profile_images.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProfileImages {
+
+  static const List<String> profilePics = [
+    'assets/pfp/1.png',
+    'assets/pfp/2.png',
+    'assets/pfp/3.png',
+    'assets/pfp/4.png',
+    'assets/pfp/5.png',
+  ];
+
+  static const List<String> bannerPics = [
+    'assets/banner/1.png',
+    'assets/banner/2.png',
+    'assets/banner/3.png',
+    'assets/banner/4.png',
+    'assets/banner/5.png',
+  ];
+
+  static const List<Color> profilePicColors = [
+    Color(0xFF2196F3),
+    Color(0xFF4CAF50),
+    Color(0xFFFFC107),
+    Color(0xFFE91E63),
+    Color(0xFF9C27B0),
+  ];
+}
